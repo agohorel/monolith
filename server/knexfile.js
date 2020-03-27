5561972a14faa52f17d53c339353ba3fc8a61463
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-03-27 18:20:11 -0400

    add db config

---
+++ server/knexfile.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: "patch_exchange",
+      port: process.env.LOCAL_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    migrations: {
+      directory: "./db/migrations"
+    },
+    seeds: {
+      directory: "./db/seeds/dev"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    useNullAsDefault: true
+  },
+
+  testing: {
+    client: "pg",
+    connection: {
+      database: "patch_exchange_testing",
+      port: process.env.LOCAL_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    migrations: {
+      directory: "./db/migrations"
+    },
+    seeds: {
+      directory: "./db/seeds/testing"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./db/migrations"
+    },
+    seeds: {
+      directory: "./db/seeds/production"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    useNullAsDefault: true
+  }
+};
