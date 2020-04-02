02b1037a7ea133312486180ac07b70892aa795b1
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-04-02 13:18:05 -0400

    add a route to get all patch metadata lists

--- server/patches/patch-router.js
+++ server/patches/patch-router.js
@@ -51,6 +51,36 @@ router.get("/status-list", async (req, res) => {
   }
 });
 
+router.get("/metadata-lists", async (req, res) => {
+  try {
+    const operatingSystems = await db.listPatchMetadata(
+      "operating_systems as os",
+      "os.os_name"
+    );
+    const platforms = await db.listPatchMetadata(
+      "platforms as p",
+      "p.platform_name"
+    );
+    const categories = await db.listPatchMetadata(
+      "categories as c",
+      "c.category_name"
+    );
+    const releaseStatuses = await db.listPatchMetadata(
+      "release_statuses as rs",
+      "rs.release_status"
+    );
+
+    res.status(200).json({
+      operatingSystems,
+      platforms,
+      categories,
+      releaseStatuses
+    });
+  } catch (error) {
+    res.status(500).json({ err: error });
+  }
+});
+
 //// * PATCH CRUD ROUTES * ////
 
 router.get("/:patchName", async (req, res) => {
