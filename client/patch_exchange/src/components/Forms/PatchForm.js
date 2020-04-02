1d95ab41e0a5017dd72a8eb6722de296015eab94
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-04-02 14:39:35 -0400

    add keys to metadata lists & fix some markup issues

--- client/patch_exchange/src/components/Forms/PatchForm.js
+++ client/patch_exchange/src/components/Forms/PatchForm.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { connect } from "react-redux";
 
 import { fetchMetadataLists } from "../../actions/patchActions";
@@ -25,7 +24,7 @@ const PatchForm = ({ metadataLists, fetchMetadataLists }) => {
 
   useEffect(() => {
     fetchMetadataLists();
-  }, []);
+  }, [fetchMetadataLists]);
 
   const handleChange = e => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -36,50 +35,87 @@ const PatchForm = ({ metadataLists, fetchMetadataLists }) => {
     // redux action here
   };
 
-  console.log(metadataLists);
-
   return (
     <Form onSubmit={handleSubmit}>
       <Label htmlFor="name">name</Label>
       <Input id="name" onChange={handleChange}></Input>
-      <Label htmlFor="image_url">image url</Label>
-      <Input id="image_url" onChange={handleChange}></Input>
-      <Label htmlFor="preview_url">preview url</Label>
-      <Input id="preview_url" onChange={handleChange}></Input>
-      <Label htmlFor="repo_url">repo url</Label>
-      <Input id="repo_url" onChange={handleChange}></Input>
+      <Label htmlFor="imageUrl">image url</Label>
+      <Input id="imageUrl" onChange={handleChange}></Input>
+      <Label htmlFor="previewUrl">preview url</Label>
+      <Input id="previewUrl" onChange={handleChange}></Input>
+      <Label htmlFor="repoUrl">repo url</Label>
+      <Input id="repoUrl" onChange={handleChange}></Input>
       <Label htmlFor="version">version name</Label>
       <Input id="version" onChange={handleChange}></Input>
-      <Label htmlFor="file_url">file url</Label>
-      <Input id="file_url" onChange={handleChange}></Input>
+      <Label htmlFor="fileUrl">file url</Label>
+      <Input id="fileUrl" onChange={handleChange}></Input>
       <Label htmlFor="description">description</Label>
       <Textarea id="description" onChange={handleChange}></Textarea>
 
       <Label htmlFor="release_status">release status</Label>
-      <Select id="release_status">
-        {metadataLists.releaseStatuses?.map(os => {
-          return <Option value={os}>{os}</Option>;
+      <Select id="release_status" onChange={handleChange}>
+        {metadataLists.releaseStatuses?.map(releaseStatus => {
+          return (
+            <Option
+              key={releaseStatus.id}
+              id={releaseStatus.release_status}
+              value={releaseStatus.release_status}
+            >
+              {releaseStatus.release_status}
+            </Option>
+          );
         })}
       </Select>
 
       <Label htmlFor="os">os compatibility</Label>
-      <Select id="os">
+      <Select id="os" onChange={handleChange}>
         {metadataLists.operatingSystems?.map(os => {
-          return <Option value={os}>{os}</Option>;
+          return (
+            <Option key={os.id} id={os.os_name} value={os.os_name}>
+              {os.os_name}
+            </Option>
+          );
         })}
       </Select>
 
-      <Label htmlFor="os">platform</Label>
-      <Select id="platform">
-        {metadataLists.platforms?.map(os => {
-          return <Option value={os}>{os}</Option>;
+      <Label htmlFor="platform">platform</Label>
+      <Select id="platforms" onChange={handleChange}>
+        {metadataLists.platforms?.map(platform => {
+          return (
+            <Option
+              key={platform.id}
+              id={platform.platform_name}
+              value={platform.platform_name}
+            >
+              {platform.platform_name}
+            </Option>
+          );
         })}
       </Select>
 
-      <Label htmlFor="os">categories</Label>
-      <Select id="categories">
-        {metadataLists.categories?.map(os => {
-          return <Option value={os}>{os}</Option>;
+      <Label htmlFor="categories">categories</Label>
+      <Select id="categories" onChange={handleChange}>
+        {metadataLists.categories?.map(category => {
+          return (
+            <Option
+              key={category.id}
+              id={category.category_name}
+              value={category.category_name}
+            >
+              {category.category_name}
+            </Option>
+          );
+        })}
+      </Select>
+
+      <Label htmlFor="tags">tags</Label>
+      <Select id="tags" onChange={handleChange}>
+        {metadataLists.tags?.map(tag => {
+          return (
+            <Option key={tag.id} id={tag.tag} value={tag.tag}>
+              {tag.tag}
+            </Option>
+          );
         })}
       </Select>
 
