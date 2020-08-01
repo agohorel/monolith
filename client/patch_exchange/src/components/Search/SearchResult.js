fea9a7936db6bf030ec011b2d2c36d082a42b1a6
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-01 16:12:34 -0400

    refactor search to use new selectPatch action, render multiple results

--- client/patch_exchange/src/components/Search/SearchResult.js
+++ client/patch_exchange/src/components/Search/SearchResult.js
@@ -13,91 +13,107 @@ import {
   ExternalLink,
 } from "./SearchStyles";
 
-const SearchResult = ({ searchResult: result, b2Auth }) => {
-  return (
-    <SearchResultContainer>
-      <Title>{result.name}</Title>
-      {result.imageId && (
-        <ImageLink to={`patches/${result.name}`}>
-          <Image
-            src={`${b2Auth.downloadUrl}/b2api/v1/b2_download_file_by_id?fileId=${result.imageId}`}
-          ></Image>
-        </ImageLink>
-      )}
+import { selectPatch } from "../../actions/patchActions";
 
-      {result.homepageUrl && (
-        <ExternalLink
-          href={result.homepageUrl}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          homepage
-        </ExternalLink>
-      )}
+const SearchResult = ({ searchResult: results, b2Auth, selectPatch }) => {
+  const handleSelect = (patch) => {
+    selectPatch(patch);
+  };
 
-      {result.previewUrl && (
-        <ExternalLink
-          href={result.previewUrl}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          preview
-        </ExternalLink>
-      )}
+  if (results.length) {
+    return (
+      <SearchResultContainer>
+        {results.map((result) => {
+          return (
+            <Result key={result.id} onClick={() => handleSelect(result)}>
+              <Title>{result.name}</Title>
+              {result.imageId && (
+                <ImageLink to={`patches/${result.name}`}>
+                  <Image
+                    src={`${b2Auth.downloadUrl}/b2api/v1/b2_download_file_by_id?fileId=${result.imageId}`}
+                  ></Image>
+                </ImageLink>
+              )}
 
-      {result.repoUrl && (
-        <ExternalLink
-          href={result.repoUrl}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          repo
-        </ExternalLink>
-      )}
+              {result.homepageUrl && (
+                <ExternalLink
+                  href={result.homepageUrl}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  homepage
+                </ExternalLink>
+              )}
 
-      {result.description && (
-        <ListItem style={{ color: "#8c8c8c" }}>{result.description}</ListItem>
-      )}
+              {result.previewUrl && (
+                <ExternalLink
+                  href={result.previewUrl}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  preview
+                </ExternalLink>
+              )}
 
-      <ListsContainer>
-        {result.platforms && (
-          <List>
-            <ListTitle>platforms</ListTitle>
-            {result.platforms?.map((platform) => (
-              <ListItem key={platform}>{platform}</ListItem>
-            ))}
-          </List>
-        )}
+              {result.repoUrl && (
+                <ExternalLink
+                  href={result.repoUrl}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  repo
+                </ExternalLink>
+              )}
 
-        {result.categories && (
-          <List>
-            <ListTitle>categories</ListTitle>
-            {result.categories?.map((category) => (
-              <ListItem key={category}>{category}</ListItem>
-            ))}
-          </List>
-        )}
+              {result.description && (
+                <ListItem style={{ color: "#8c8c8c" }}>
+                  {result.description}
+                </ListItem>
+              )}
 
-        {result.operatingSystems && (
-          <List>
-            <ListTitle>operating systems</ListTitle>
-            {result.operatingSystems?.map((os) => (
-              <ListItem key={os}>{os}</ListItem>
-            ))}
-          </List>
-        )}
+              <ListsContainer>
+                {result.platforms && (
+                  <List>
+                    <ListTitle>platforms</ListTitle>
+                    {result.platforms?.map((platform) => (
+                      <ListItem key={platform}>{platform}</ListItem>
+                    ))}
+                  </List>
+                )}
 
-        {result.tags && (
-          <List>
-            <ListTitle>tags</ListTitle>
-            {result.tags?.map((tag) => (
-              <ListItem key={tag}>{tag}</ListItem>
-            ))}
-          </List>
-        )}
-      </ListsContainer>
-    </SearchResultContainer>
-  );
+                {result.categories && (
+                  <List>
+                    <ListTitle>categories</ListTitle>
+                    {result.categories?.map((category) => (
+                      <ListItem key={category}>{category}</ListItem>
+                    ))}
+                  </List>
+                )}
+
+                {result.operatingSystems && (
+                  <List>
+                    <ListTitle>operating systems</ListTitle>
+                    {result.operatingSystems?.map((os) => (
+                      <ListItem key={os}>{os}</ListItem>
+                    ))}
+                  </List>
+                )}
+
+                {result.tags && (
+                  <List>
+                    <ListTitle>tags</ListTitle>
+                    {result.tags?.map((tag) => (
+                      <ListItem key={tag}>{tag}</ListItem>
+                    ))}
+                  </List>
+                )}
+              </ListsContainer>
+            </Result>
+          );
+        })}
+      </SearchResultContainer>
+    );
+  } else return null;
 };
 
 const mapStateToProps = (state) => {
@@ -107,7 +123,9 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, {})(SearchResult);
+export default connect(mapStateToProps, { selectPatch })(SearchResult);
+
+const Result = styled.div``;
 
 const ImageLink = styled(Link)`
   display: flex;
