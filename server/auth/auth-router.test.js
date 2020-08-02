527eb3fe198b14c05e86c4363a1c6de42d3da3f8
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-02 14:50:37 -0400

    add user registration tests

--- server/auth/auth-router.test.js
+++ server/auth/auth-router.test.js
@@ -1,7 +1,18 @@
+/**
+ * @jest-environment node
+ */
+
 const db = require("../db/connections");
-const { generateToken } = require("../utils/tokenUtils");
+const dbModel = require("../db/dbModel");
+const bcrypt = require("bcryptjs");
+const { generateToken, hashPassword } = require("../utils/authUtils");
 const server = require("../api/server");
 const request = require("supertest");
+
+afterAll((done) => {
+  dbModel.closeConnection();
+  done();
+});
 
 describe("Environment test", () => {
   it("Should be using the test environment", () => {
@@ -20,5 +31,51 @@ describe("Environment test", () => {
   it("Should have B2 credentials in .env", () => {
     expect(process.env.B2_KEY_ID).toBeDefined();
     expect(process.env.B2_APPLICATION_KEY).toBeDefined();
+  });
+});
+
+describe("User Registration tests", () => {
+  beforeEach(async () => {
+    await db("users").delete();
+  });
+
+  const password = "quake777!";
+  const testUser = {
+    username: "johndoe",
+    password: hashPassword(password),
+    email: "jdoe@email.net",
+    bio: "hello world",
+  };
+
+  it("Should add a user to the database", async () => {
+    await dbModel.insert("users", testUser, "id");
+    const users = await dbModel.find("users");
+
+    expect(users).toEqual(
+      expect.arrayContaining([expect.objectContaining(testUser)])
+    );
+  });
+
+  describe("User Registration via POST api/auth/register", () => {
+    it("Responds with 201 and persists user to DB", async () => {
+      const response = await request(server)
+        .post("/api/auth/register")
+        .send({
+          ...testUser,
+          password,
+        });
+      const users = await dbModel.find("users");
+
+      expect(response.status).toEqual(201);
+      expect(users).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            username: testUser.username,
+            email: testUser.email,
+            bio: testUser.bio,
+          }),
+        ])
+      );
+    });
   });
 });
