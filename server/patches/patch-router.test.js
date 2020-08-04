b26c708f82aaa0e85b9effc84dc03ab556ead18b
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-04 17:04:52 -0400

    add patch CRUD tests, resolve teardown warning

--- server/patches/patch-router.test.js
+++ server/patches/patch-router.test.js
@@ -1,8 +1,16 @@
 /**
  * @jest-environment node
  */
+
+const db = require("../db/connections");
+const dbModel = require("../db/dbModel");
 const server = require("../api/server");
 const request = require("supertest");
+
+afterAll((done) => {
+  dbModel.closeConnection();
+  done();
+});
 
 describe("Patch metadata list tests", () => {
   it("Should return a valid list of operating systems", async () => {
@@ -44,6 +52,71 @@ describe("Patch metadata list tests", () => {
     for (category of categories) {
       await testMetadataRoute(category.endpoint, category.target_field);
     }
+  });
+});
+
+describe("Patch CRUD route tests", () => {
+  beforeAll(async () => {
+    await db.raw("TRUNCATE TABLE versions, patches RESTART IDENTITY CASCADE");
+    await db.migrate.latest().then(() => db.seed.run());
+  });
+
+  it("POST - Should create a patch", async () => {
+    const patch = {
+      user_id: 1,
+      user_name: "johndoe",
+      name: "shred12",
+      image_url: "www.image.com",
+      preview_url: "www.preview.com",
+      repo_url: "www.repo.com",
+      homepage_url: "www.homepage.com",
+      version: "1.0",
+      file_url: "www.file.com",
+      description: "1.0 release woot",
+      releaseStatuses: "1",
+      operatingSystems: ["1", "2"],
+      platforms: ["1"],
+      categories: ["2"],
+      tags: ["1", "2"],
+    };
+
+    const res = await request(server)
+      .post("/api/patches/add-patch")
+      .send(patch);
+
+    expect(res.status).toEqual(201);
+    expect(res.body.msg).toContain("Successfully created patch");
+  });
+
+  it("GET - Should return a patch by ID", async () => {
+    const res = await request(server).get("/api/patches/1");
+    expect(res.status).toEqual(200);
+    expect(res.body.name).toBeDefined();
+    expect(res.body.authorName).toBeDefined();
+    expect(res.body.description).toBeDefined();
+  });
+
+  it("PUT - should update a patch", async () => {
+    const patchData = {
+      name: "testpatch ULTIMATE EDITION",
+      operatingSystems: ["4", "5"],
+      platforms: ["1"],
+      categories: ["1"],
+      tags: ["1", "4"],
+    };
+
+    const res = await request(server).put(`/api/patches/1`).send(patchData);
+
+    expect(res.status).toEqual(200);
+    expect(res.body.msg).toContain("Successfully updated patch");
+  });
+
+  it("DELETE - should remove a patch", async () => {
+    const res = await request(server).delete("/api/patches/1");
+    const patch = await dbModel.findBy("patches", { id: 1 });
+  
+    expect(res.status).toEqual(200);
+    expect(patch).toBeUndefined();
   });
 });
 
